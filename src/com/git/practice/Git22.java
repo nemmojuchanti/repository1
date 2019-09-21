cb0a72a4ed61e1c3288a3407db68d54948067b43
nemmojuchanti <nemmoju.chanti@gmail.com>
2019-09-21 16:13:01 +0530

    third commit

---
+++ src/com/git/practice/Git22.java
@@ -0,0 +1,13 @@
+package com.git.practice;
+
+public class Git22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("giittt");
+		
+		
+	}
+
+}
